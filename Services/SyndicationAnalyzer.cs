a321fb441c654033fe1cb6979211490d845f48eb
damien.galban <damien.galban@gmail.com>
2015-07-13 13:18:12 +0200

    Fixed issue #5

--- Services/SyndicationAnalyzer.cs
+++ Services/SyndicationAnalyzer.cs
@@ -17,8 +17,28 @@ namespace PodCatchup.Services
       var xml = XDocument.Load(podcast.Syndication);
       XNamespace ns = "http://www.itunes.com/dtds/podcast-1.0.dtd";
       podcast.Title = xml.Element("rss").Element("channel").Element("title").Value;
-      podcast.Cover = xml.Element("rss").Element("channel").Element("image").Element("url").Value;
-      podcast.Summary = xml.Element("rss").Element("channel").Element(ns + "summary").Value;
+      XElement coverelem = xml.Element("rss").Element("channel").Element("image");
+      if (coverelem == null)
+      {
+        podcast.Cover = xml.Element("rss").Element("channel").Element(ns + "image").Attribute("href").Value;
+      }
+      else
+      {
+        podcast.Cover = coverelem.Element("url").Value;
+      }
+      var summaryElement = xml.Element("rss").Element("channel").Element(ns + "summary");
+      if (summaryElement == null)
+      {
+        summaryElement = xml.Element("rss").Element("channel").Element(ns + "subtitle");
+      }
+      if (summaryElement == null)
+      {
+        summaryElement = xml.Element("rss").Element("channel").Element("description");
+      }
+      if (summaryElement != null)
+      {
+        podcast.Summary = summaryElement.Value;
+      }
 
       loadItemsFromFeed(ref podcast, ref xml, ref ns);
     }
@@ -40,7 +60,10 @@ namespace PodCatchup.Services
         nep.Summary = item.Element(ns + "summary").Value;
         nep.Subtitle = item.Element(ns + "subtitle").Value;
         nep.Url = item.Element("enclosure").Attribute("url").Value;
-        nep.Duration = item.Element(ns + "duration").Value;
+        if (item.Element(ns + "duration") != null)
+        {
+          nep.Duration = item.Element(ns + "duration").Value;
+        }
         nep.Guid = item.Element("guid").Value;
         nep.Published = DateTime.Parse(item.Element("pubDate").Value);
         if (item.Element(ns + "image") != null)
